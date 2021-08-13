bd018678668e68bec9bd771c16d53c500fd0d1fb
陈增 <zeng.chen@geely.com>
2021-08-13 16:29:54 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        sidebar: 'auto',
+        smoothScroll: true, // 页面滚动效果
+
+        /*sidebar: [
+            '/',
+            {
+                title: '分组一',
+                children : [
+                    '/OWL前端架构',
+                    ['/会议记录', 'hhhh']
+                ]
+            },
+        ],*/
+        // displayAllHeaders: false, // 显示所有页面的标题链接
+        // activeHeaderLinks: true, // 活动的标题链接
+    }
+}
