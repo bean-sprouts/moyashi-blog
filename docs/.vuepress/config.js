a04b9dc42dd18ac13e825bb1f37ae49cec049e84
陈增 <zeng.chen@geely.com>
2021-08-13 18:23:08 +0800

    feat(nav): 优化导航栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,18 +3,13 @@ module.exports = {
     title: '思考的豆芽',
     description: '技术分享博客',
     themeConfig: {
-        sidebar: 'auto',
         smoothScroll: true, // 页面滚动效果
-        /*sidebar: [
-            '/',
-            {
-                title: '分组一',
-                children : [
-                    '/OWL前端架构',
-                    ['/会议记录', 'hhhh']
-                ]
-            },
-        ],*/
+        nav: [{text: "主页", link: "/"      },
+            { text: "工作", link: "/work/新技术学习" },
+        ],
+        sidebar: {
+            '/work/': ['新技术学习']
+        },
         // displayAllHeaders: false, // 显示所有页面的标题链接
         // activeHeaderLinks: true, // 活动的标题链接
     }
--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ heroImage: null
 heroText: Hero 标题
 tagline: Hero 副标题
 actionText: 快速上手 →
-actionLink: /OWL前端架构
+actionLink: /work/新技术学习
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
